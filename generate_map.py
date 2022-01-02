fd5f92ca1015fcc1a53aeb1782ccbcbf3e9ae32c
KostyaKostev <kakoyedelo@bk.ru>
2022-01-02 16:32:58 +0300

    разделил обьекты на классы для более удобной работы

--- generate_map.py
+++ generate_map.py
@@ -6,19 +6,21 @@ import sys
 
 COLORS = ['floor', 'plant', 'floor', 'floor', 'cupboard', 'floor', 'floor', 'floor', 'table']
 all_sprites = pygame.sprite.Group()
+furniture_sprites = pygame.sprite.Group()
+plant_sprites = pygame.sprite.Group()
 surf = pygame.Surface((1060, 560))
 
 
 def load_image(name, colorkey=None):
-    fullname = "data/" + name
+    fullname = 'data/' + name
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     return image
 
-
-class object_sprite(pygame.sprite.Sprite):
+# Костя К
+class floor(pygame.sprite.Sprite):
     def __init__(self, image_name, x, y):
         super().__init__(all_sprites)
         self.image = load_image(image_name)
@@ -29,7 +31,41 @@ class object_sprite(pygame.sprite.Sprite):
     def update(self):
         pass
 
+    # никак не взаимодействует - поэтому нет коллизии
 
+
+
+class plant_sprite(pygame.sprite.Sprite):
+    def __init__(self, image_name, x, y):
+        super().__init__(plant_sprites)
+        self.image = load_image(image_name)
+        self.rect = self.image.get_rect()
+        self.cur_frame = 0
+        self.rect = self.rect.move(x, y)
+
+    def update(self):
+        pass
+
+    def collision(self, object):
+        # предполагаю, что растение будет менять текстуру на разбитую при столкновении с пулей
+        pass
+
+
+class furniture_sprite(pygame.sprite.Sprite):
+    def __init__(self, image_name, x, y):
+        super().__init__(furniture_sprites)
+        self.image = load_image(image_name)
+        self.rect = self.image.get_rect()
+        self.cur_frame = 0
+        self.rect = self.rect.move(x, y)
+
+    def update(self):
+        pass
+
+    def collision(self, object):
+        pass
+
+# -----------------
 class Board:
     def __init__(self, width, height, left=10, top=10, cell_size=30):
         # Cаша Ч
@@ -43,7 +79,7 @@ class Board:
                     or i == height - 1 and j == width - 1:
                     board_row.append(randint(0, 1))
                 elif i == 0 or i == height - 1:
-                    board_row.append(randint(2, 4))
+                    board_row.append(randint(2, 5))
                 elif abs(i - height // 2) <= 2 and abs(j - width / 2) <= 1:
                     board_row.append((randint(5, 8)))
                 else:
@@ -64,17 +100,19 @@ class Board:
                 pygame.draw.rect(screen, 'black', (self.left + col * self.cell_size,
                         self.top + row * self.cell_size, self.cell_size, self.cell_size), 1)
                 if COLORS[self.board[row][col]] == 'cupboard':
-                    current_object = object_sprite('cupboard.xcf', self.left + col * self.cell_size + 1,
+                    current_object = furniture_sprite('cupboard.xcf', self.left + col * self.cell_size + 1,
                                                                         self.top + row * self.cell_size + 1)
                 elif COLORS[self.board[row][col]] == 'floor':
-                    current_object = object_sprite('floor.xcf', self.left + col * self.cell_size + 1,
+                    current_object = floor('floor.xcf', self.left + col * self.cell_size + 1,
                                                    self.top + row * self.cell_size + 1)
                 elif COLORS[self.board[row][col]] == 'table':
-                    current_object = object_sprite('table.xcf', self.left + col * self.cell_size + 1,
+                    current_object = furniture_sprite('table.xcf', self.left + col * self.cell_size + 1,
                                                    self.top + row * self.cell_size + 1)
                 elif COLORS[self.board[row][col]] == 'plant':
-                    current_object = object_sprite('plant.xcf', self.left + col * self.cell_size + 1,
+                    current_object = plant_sprite('plant.xcf', self.left + col * self.cell_size + 1,
                                                    self.top + row * self.cell_size + 1)
+        furniture_sprites.draw(screen)
+        plant_sprites.draw(screen)
         all_sprites.draw(screen)
 
 
