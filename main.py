92a58b5beae2558eab9b4fa4b3f53b076e217e29
AlexanderChck <92948608+AlexanderChck@users.noreply.github.com>
2022-01-07 12:58:56 +0300

    добавлен экран выбора класса

--- main.py
+++ main.py
@@ -1,58 +1,174 @@
+# Талдин М.
 import pygame
-
-from player import *
 from decor import *
-
-
-def init(check, pl=None, board=None, cur_ind=None, enemies=None):
-    pass
+from player import *
 
 
 pygame.init()
 size = WIDTH, HEIGHT = 1060, 560
 FPS = 120
+LEFT = 30
+TOP = 30
+CELL_SIZE = 50
+
 screen = pygame.display.set_mode(size)
-all_sprites = pygame.sprite.Group()
-enemies = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
+menu_sprites = pygame.sprite.Group()
+class_sprites = pygame.sprite.Group()
 clock = pygame.time.Clock()
 room_sprites = get_decorate()
-for group in room_sprites:
-    for sprite in group:
-        all_sprites.add(sprite)
-running = True
-pl = create_player(WIDTH // 2, HEIGHT // 2, all_sprites, enemies, bullets, size)
-for coords in [(0, 0), (WIDTH - 70, 0), (0, HEIGHT - 70), (WIDTH, HEIGHT)]:
-    create_enemy(coords[0], coords[1], pl, all_sprites, enemies, bullets)
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or not pl.alive():
-            running = False
-        if event.type == NEW_ROOM:
-            if pl.rect.x - LEFT <= CELL_SIZE:
-                if cur_ind != 0:
-                    cur_ind -= 1
-                else:
-                    continue
-            else:
-                cur_ind += 1
-            if cur_ind >= len(boards):
-                board = Board(20, 10)
-                boards.append(board)
-            cur_board = boards[cur_ind]
-            for sprite in all_sprites:
-                sprite.kill()
-            cur_board.render()
-            room_sprites = get_decorate()
-            for group in room_sprites:
-                for sprite in group:
-                    all_sprites.add(sprite)
-            pl = create_player(WIDTH // 2, HEIGHT // 2, all_sprites, enemies, bullets, size)
-            for coords in [(0, 0), (WIDTH - 70, 0), (0, HEIGHT - 70), (WIDTH, HEIGHT)]:
-                create_enemy(coords[0], coords[1], pl, all_sprites, enemies, bullets)
-    screen.fill((0, 0, 0))
+
+
+def start_game():
+    global cur_ind
+    global LEFT, TOP, CELL_SIZE
+    running = True
+    all_sprites = pygame.sprite.Group()
+    enemies = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    room_sprites = get_decorate()
+    for group in room_sprites:
+        for sprite in group:
+            all_sprites.add(sprite)
+    pl = create_player(WIDTH // 2, HEIGHT // 2, player_group, enemies, bullets, size)
     all_sprites.draw(screen)
-    all_sprites.update()
-    pygame.display.flip()
-    clock.tick(FPS)
-pygame.quit()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == NEW_ROOM:
+                new = False
+                if pl.rect.x - LEFT <= CELL_SIZE:
+                    if cur_ind != 0:
+                        cur_ind -= 1
+                    else:
+                        continue
+                else:
+                    cur_ind += 1
+                    new = True
+                if cur_ind >= len(boards):
+                    board = Board(20, 10)
+                    boards.append(board)
+                cur_board = boards[cur_ind]
+                for sprite in all_sprites:
+                    sprite.kill()
+                cur_board.render()
+                room_sprites = get_decorate()
+                for group in room_sprites:
+                    for sprite in group:
+                        all_sprites.add(sprite)
+                pl.rect.center = WIDTH // 2, HEIGHT // 2
+                if new:
+                    for coords in [(0, 0), (WIDTH - 70, 0), (0, HEIGHT - 70), (WIDTH, HEIGHT)]:
+                        create_enemy(coords[0], coords[1], pl, all_sprites, enemies, bullets)
+        if not pl.alive():
+            running = False
+            for sprited in all_sprites:
+                all_sprites.remove(sprited)
+            create_menu()
+        if running:
+            screen.fill((0, 0, 0))
+            all_sprites.draw(screen)
+            all_sprites.update()
+            player_group.update()
+            player_group.draw(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+
+
+def select_class():
+    running = True
+    menu_sprite_background = pygame.sprite.Sprite(class_sprites)
+    menu_sprite_background.image = load_image('menu_image.png')
+    menu_sprite_background.rect = menu_sprite_background.image.get_rect()
+    warrior = pygame.sprite.Sprite(class_sprites)
+    warrior.image = load_image('warrior.PNG')
+    warrior.rect = warrior.image.get_rect()
+    warrior.rect.x = 40
+    warrior.rect.y = 205
+    archer = pygame.sprite.Sprite(class_sprites)
+    archer.image = load_image('archer.PNG')
+    archer.rect = archer.image.get_rect()
+    archer.rect.x = 380
+    archer.rect.y = 205
+    wizard = pygame.sprite.Sprite(class_sprites)
+    wizard.image = load_image('wizard.PNG')
+    wizard.rect = wizard.image.get_rect()
+    wizard.rect.x = 720
+    wizard.rect.y = 205
+    cursor_sprite = pygame.sprite.Sprite(class_sprites)
+    cursor_sprite.image = load_image('cursor_image.png')
+    cursor_sprite.rect = cursor_sprite.image.get_rect()
+    coords = pygame.mouse.get_pos()
+    cursor_sprite.rect.center = coords
+    class_sprites.draw(screen)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == pygame.MOUSEMOTION:
+                coords = pygame.mouse.get_pos()
+                cursor_sprite.rect.x, cursor_sprite.rect.y = coords
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.sprite.collide_rect(cursor_sprite, warrior):
+                    running = False
+                    for spritet in class_sprites:
+                        spritet.kill()
+                    return 'warrior'
+                elif pygame.sprite.collide_rect(cursor_sprite, archer):
+                    running = False
+                    for spritet in class_sprites:
+                        spritet.kill()
+                    return 'archer'
+                elif pygame.sprite.collide_rect(cursor_sprite, wizard):
+                    running = False
+                    for spritet in class_sprites:
+                        spritet.kill()
+                    return 'wizard'
+        if running:
+            screen.fill((0, 0, 0))
+            class_sprites.draw(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+
+
+# Костя К.
+def create_menu():
+    running = True
+    menu_sprite_background = pygame.sprite.Sprite(menu_sprites)
+    menu_sprite_background.image = load_image('menu_image.png')
+    menu_sprite_background.rect = menu_sprite_background.image.get_rect()
+    button_sprite = pygame.sprite.Sprite(menu_sprites)
+    button_sprite.image = load_image('main_text.xcf')
+    button_sprite.rect = button_sprite.image.get_rect()
+    button_sprite.rect.x = 250
+    button_sprite.rect.y = 120
+    pygame.mouse.set_visible(False)
+    cursor_sprite = pygame.sprite.Sprite(menu_sprites)
+    cursor_sprite.image = load_image('cursor_image.png')
+    cursor_sprite.rect = cursor_sprite.image.get_rect()
+    coords = pygame.mouse.get_pos()
+    cursor_sprite.rect.center = coords
+    menu_sprites.draw(screen)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+            if event.type == pygame.MOUSEMOTION:
+                coords = pygame.mouse.get_pos()
+                cursor_sprite.rect.x, cursor_sprite.rect.y = coords
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if pygame.sprite.collide_rect(cursor_sprite, button_sprite):
+                    running = False
+                    for spritet in menu_sprites:
+                        spritet.kill()
+                    select_class()
+        if running:
+            screen.fill((0, 0, 0))
+            menu_sprites.draw(screen)
+            pygame.display.flip()
+            clock.tick(FPS)
+create_menu()
