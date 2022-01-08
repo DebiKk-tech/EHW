f7804dbbb15ad25fe5bf2b752ed21aef52db783a
AlexanderChck <92948608+AlexanderChck@users.noreply.github.com>
2022-01-08 19:53:03 +0300

    добавлен экран загрузки своей локации

--- main.py
+++ main.py
@@ -1,55 +1,98 @@
-# Талдин М.
 import pygame
 from decor import *
 from player import *
+from pygame.constants import K_e
+from walls import *
+from Death import death
+from add_room import *
 
 
 pygame.init()
-size = WIDTH, HEIGHT = 1060, 560
+WIDTH, HEIGHT = 1060, 560
+size = WIDTH, HEIGHT + 50  # Добавляется 50-пиксельное пространство для интерфейса, которое остальная игра не учитывает
 FPS = 120
 LEFT = 30
 TOP = 30
 CELL_SIZE = 50
 
 screen = pygame.display.set_mode(size)
+size = WIDTH, HEIGHT
 menu_sprites = pygame.sprite.Group()
 class_sprites = pygame.sprite.Group()
 clock = pygame.time.Clock()
 room_sprites = get_decorate()
 
 
-def start_game():
+def render_text(x, y, text, surface):
+    font = pygame.font.Font(None, 50)
+    text = font.render(text, True, (255, 255, 255))
+    text_x = x
+    text_y = y
+    surface.blit(text, (text_x, text_y))
+
+
+def start_game(player_class):
     global cur_ind
     global LEFT, TOP, CELL_SIZE
     running = True
     all_sprites = pygame.sprite.Group()
     enemies = pygame.sprite.Group()
+    enemies_bullets = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
     player_group = pygame.sprite.Group()
+    walls = pygame.sprite.Group()
+    doors = pygame.sprite.Group()
+    wall1 = Wall(0, 0, 1060, 31, [walls, player_group])
+    wall2 = Wall(0, 0, 31, 560, [walls, player_group])
+    wall3 = Wall(1030, 0, 31, 560, [walls, player_group])
+    wall4 = Wall(0, 530, 1060, 31, [walls, player_group])
+    door1 = Door(size, 'left', [doors])
+    door1.set_closed()
+    door2 = Door(size, 'right', [doors])
+    DECREASE_POINTS_EVENT = pygame.USEREVENT + 2
+    pygame.time.set_timer(DECREASE_POINTS_EVENT, 360)
     room_sprites = get_decorate()
     for group in room_sprites:
         for sprite in group:
             all_sprites.add(sprite)
-    pl = create_player(WIDTH // 2, HEIGHT // 2, player_group, enemies, bullets, size)
+    pl = create_player(WIDTH // 2, HEIGHT // 2, player_group, enemies, bullets, size, player_class, all_sprites, walls,
+                       doors)
+    if pl.type == 'warrior':
+        sword = Sword(player_group, pl, 'top')
     all_sprites.draw(screen)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
+            if event.type == DECREASE_POINTS_EVENT:
+                pl.points -= 1
             if event.type == NEW_ROOM:
                 new = False
-                if pl.rect.x - LEFT <= CELL_SIZE:
+                if pl.rect.x - LEFT <= CELL_SIZE * 3:
                     if cur_ind != 0:
                         cur_ind -= 1
+                        if cur_ind == 0:
+                            door1.set_closed()
+                        else:
+                            door1.set_opened()
+                            door2.set_closed()
+                        pl.rect.center = WIDTH - 128, HEIGHT // 2
                     else:
                         continue
                 else:
                     cur_ind += 1
-                    new = True
+                    pl.rect.center = 128, HEIGHT // 2
+                    door1.set_opened()
                 if cur_ind >= len(boards):
+                    door1.set_closed()
                     board = Board(20, 10)
                     boards.append(board)
+                    new = True
+                    pl.rooms += 1
+                    pl.points += 90
+                    for door in doors:
+                        door.set_closed()
                 cur_board = boards[cur_ind]
                 for sprite in all_sprites:
                     sprite.kill()
@@ -58,21 +101,33 @@ def start_game():
                 for group in room_sprites:
                     for sprite in group:
                         all_sprites.add(sprite)
-                pl.rect.center = WIDTH // 2, HEIGHT // 2
                 if new:
-                    for coords in [(0, 0), (WIDTH - 70, 0), (0, HEIGHT - 70), (WIDTH, HEIGHT)]:
-                        create_enemy(coords[0], coords[1], pl, all_sprites, enemies, bullets)
+                    for coords in [(100, 100), (WIDTH - 100, 100), (100, HEIGHT - 100), (WIDTH - 100, HEIGHT - 100)]:
+                        create_enemy(coords[0], coords[1], pl, all_sprites, enemies, bullets, player_group,
+                                     'shooting-left', enemies_bullets)
+        if pygame.key.get_pressed()[K_e] and pl.reload <= 0:
+            sword.swinging = -1
+            pl.reload = 30
         if not pl.alive():
             running = False
             for sprited in all_sprites:
                 all_sprites.remove(sprited)
+            death(pl)
             create_menu()
+            break
+        if len(enemies) == 0:
+            door1.set_opened()
+            door2.set_opened()
         if running:
             screen.fill((0, 0, 0))
+            doors.draw(screen)
+            doors.update()
             all_sprites.draw(screen)
             all_sprites.update()
             player_group.update()
             player_group.draw(screen)
+            render_text(20, 570, 'Ваше здоровье: ' + str(pl.health), screen)
+            render_text(500, 570, 'Ваши очки: ' + str(pl.points), screen)
             pygame.display.flip()
             clock.tick(FPS)
 
@@ -83,17 +138,20 @@ def select_class():
     menu_sprite_background.image = load_image('menu_image.png')
     menu_sprite_background.rect = menu_sprite_background.image.get_rect()
     warrior = pygame.sprite.Sprite(class_sprites)
-    warrior.image = load_image('warrior.PNG')
+    warrior.image = pygame.Surface((40, 40))
+    warrior.image.fill((255, 0, 0))
     warrior.rect = warrior.image.get_rect()
     warrior.rect.x = 40
     warrior.rect.y = 205
     archer = pygame.sprite.Sprite(class_sprites)
-    archer.image = load_image('archer.PNG')
+    archer.image = pygame.Surface((40, 40))
+    archer.image.fill((0, 255, 0))
     archer.rect = archer.image.get_rect()
     archer.rect.x = 380
     archer.rect.y = 205
     wizard = pygame.sprite.Sprite(class_sprites)
-    wizard.image = load_image('wizard.PNG')
+    wizard.image = pygame.Surface((40, 40))
+    wizard.image.fill((0, 0, 255))
     wizard.rect = wizard.image.get_rect()
     wizard.rect.x = 720
     wizard.rect.y = 205
@@ -107,7 +165,7 @@ def select_class():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
+                sys.exit()
             if event.type == pygame.MOUSEMOTION:
                 coords = pygame.mouse.get_pos()
                 cursor_sprite.rect.x, cursor_sprite.rect.y = coords
@@ -127,6 +185,8 @@ def select_class():
                     for spritet in class_sprites:
                         spritet.kill()
                     return 'wizard'
+
+                    # start_game()
         if running:
             screen.fill((0, 0, 0))
             class_sprites.draw(screen)
@@ -145,6 +205,11 @@ def create_menu():
     button_sprite.rect = button_sprite.image.get_rect()
     button_sprite.rect.x = 250
     button_sprite.rect.y = 120
+    add_loc_btn = pygame.sprite.Sprite(menu_sprites)
+    add_loc_btn.image = load_image('your_room.png')
+    add_loc_btn.rect = add_loc_btn.image.get_rect()
+    add_loc_btn.rect.x = 250
+    add_loc_btn.rect.y = 320
     pygame.mouse.set_visible(False)
     cursor_sprite = pygame.sprite.Sprite(menu_sprites)
     cursor_sprite.image = load_image('cursor_image.png')
@@ -165,7 +230,12 @@ def create_menu():
                     running = False
                     for spritet in menu_sprites:
                         spritet.kill()
-                    select_class()
+                    start_game(select_class())
+                if pygame.sprite.collide_rect(cursor_sprite, add_loc_btn):
+                    running = False
+                    for spritet in menu_sprites:
+                        spritet.kill()
+                    your_room()
         if running:
             screen.fill((0, 0, 0))
             menu_sprites.draw(screen)
